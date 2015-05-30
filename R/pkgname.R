0fcb0d9db34da9559380edb33ed41e19da593db3
Zacharias Steinmetz <steinmetz@posteo.de>
2015-05-30 23:14:14 +0200

    Updated roxygen docs

---
+++ R/pkgname.R
@@ -0,0 +1,18 @@
+#' @title Interface to SWAP hydrological model
+#' @aliases swap2r-package
+#'
+#' @description
+#' Utility functions to read SWAP output as well as read and
+#' write its configuration files via TTutil Fortran library.
+#' 
+#' @details
+#' \tabular{ll}{
+#' Version: \tab 0.1\cr
+#' Licence: \tab GPLv3, also see LICENSE.txt for underlying TTutil\cr
+#' }
+#'
+#' @author Mikhail Titov (\email{mlt@@gmx.us})
+#'
+#' @docType package
+#' @name swap2r
+NULL
